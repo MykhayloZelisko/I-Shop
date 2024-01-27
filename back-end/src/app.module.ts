cb4b4c25355c700686465ff353ca6848aa7d814d
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2024-01-27 21:41:02 +0200

    fix app module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,16 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { ArticleModule } from './article/article.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://FantomAS:WFZ75gapcMpXssvP@i-shop.ig26m8z.mongodb.net/?retryWrites=true&w=majority',
     ),
-    UserModule,
-    ArticleModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],

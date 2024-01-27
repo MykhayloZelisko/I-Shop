4f9c5e277585c4a5f12941df7ae15ef6edb6ec1e
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2024-01-27 21:34:00 +0200

    add mongodb, passport.js, cookies, graphql

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://FantomAS:WFZ75gapcMpXssvP@i-shop.ig26m8z.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    UserModule,
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

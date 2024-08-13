cab33e4e20212472155a3c84f1ec4e90d3ac472a
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2024-08-13 23:02:09 +0300

    fix brend list component

--- client/src/app/admin/pages/brands/components/brands-list/brands-list.component.ts
+++ client/src/app/admin/pages/brands/components/brands-list/brands-list.component.ts
@@ -1,7 +1,7 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  inject,
+  inject, OnDestroy,
   OnInit,
 } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -11,6 +11,7 @@ import { State } from '../../../../../+store/reducers';
 import { selectAllBrands } from '../../../../../+store/brands/selectors/brand.selectors';
 import { AsyncPipe } from '@angular/common';
 import { BrandItemComponent } from './components/brand-item/brand-item.component';
+import { BrandActions } from '../../../../../+store/brands/actions/brand.actions';
 
 @Component({
   selector: 'app-brands-list',
@@ -20,12 +21,16 @@ import { BrandItemComponent } from './components/brand-item/brand-item.component
   styleUrl: './brands-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BrandsListComponent implements OnInit {
+export class BrandsListComponent implements OnInit, OnDestroy {
   public brands$!: Observable<BrandInterface[]>;
 
   private store = inject(Store<State>);
 
   public ngOnInit(): void {
     this.brands$ = this.store.select(selectAllBrands);
+  }
+
+  public ngOnDestroy(): void {
+    this.store.dispatch(BrandActions.clearCurrentBrandId());
   }
 }

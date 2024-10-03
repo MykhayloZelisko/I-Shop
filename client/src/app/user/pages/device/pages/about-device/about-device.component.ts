66f88a57cba6159c19ad05f4e126a268381dcd86
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2024-10-03 20:19:25 +0300

    add carousel component

--- client/src/app/user/pages/device/pages/about-device/about-device.component.ts
+++ client/src/app/user/pages/device/pages/about-device/about-device.component.ts
@@ -9,14 +9,20 @@ import { State } from '../../../../../+store/reducers';
 import { selectDevice } from '../../../../../+store/devices/selectors/device.selectors';
 import { Observable } from 'rxjs';
 import { DeviceInterface } from '../../../../../shared/models/interfaces/device.interface';
-import { AsyncPipe, NgClass, NgStyle } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { SvgIconComponent } from 'angular-svg-icon';
 import { RatingComponent } from '../../../../../shared/components/rating/rating.component';
+import { DeviceCarouselComponent } from './components/device-carousel/device-carousel.component';
 
 @Component({
   selector: 'app-about-device',
   standalone: true,
-  imports: [AsyncPipe, NgStyle, SvgIconComponent, NgClass, RatingComponent],
+  imports: [
+    AsyncPipe,
+    SvgIconComponent,
+    RatingComponent,
+    DeviceCarouselComponent,
+  ],
   templateUrl: './about-device.component.html',
   styleUrl: './about-device.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -24,31 +30,9 @@ import { RatingComponent } from '../../../../../shared/components/rating/rating.
 export class AboutDeviceComponent implements OnInit {
   public device$!: Observable<DeviceInterface | null>;
 
-  public currentImageIndex = 0;
-
   private store = inject(Store<State>);
 
   public ngOnInit(): void {
     this.device$ = this.store.select(selectDevice);
-  }
-
-  public prevImage(device: DeviceInterface): void {
-    if (this.currentImageIndex > 0) {
-      this.currentImageIndex--;
-    } else {
-      this.currentImageIndex = device.images.length - 1;
-    }
-  }
-
-  public nextImage(device: DeviceInterface): void {
-    if (this.currentImageIndex < device.images.length - 1) {
-      this.currentImageIndex++;
-    } else {
-      this.currentImageIndex = 0;
-    }
-  }
-
-  public setCurrentIndex(index: number): void {
-    this.currentImageIndex = index;
   }
 }
---
+++ client/src/app/user/pages/device/pages/about-device/components/device-carousel/device-carousel.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeviceCarouselComponent } from './device-carousel.component';
+
+describe('DeviceCarouselComponent', () => {
+  let component: DeviceCarouselComponent;
+  let fixture: ComponentFixture<DeviceCarouselComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DeviceCarouselComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(DeviceCarouselComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
